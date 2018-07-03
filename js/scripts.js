dec6ca06d28e4d81dc9ebde98b3e99fb3231346b
Valera <jw2@mail.ru>
2018-07-03 18:12:30 +0400

    attraction

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,30 @@
 $(document).ready(function () {
 
-   $('.slider-init').slick({
-  centerMode: true,
-  centerPadding: '0',
-  slidesToShow: 3,
-       dots: true
+	$('.slider-init').slick({
+		centerMode: true,
+		centerPadding: '0',
+		slidesToShow: 3,
+	    dots: true
+	});
+    
+    $('.attraction__slider-for').slick({
+      	slidesToShow: 1,
+	    slidesToScroll: 1,
+	    arrows: false,
+	    fade: false,
+	    asNavFor: '.attraction__slider-nav',
+	    infinite: false
+    });
 
-});
-    
-    
+    $('.attraction__slider-nav').slick({
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        asNavFor: '.attraction__slider-for',
+        infinite: false,
+        dots: false,
+        centerMode: false,
+        focusOnSelect: true,
+    });
     
 
 });
