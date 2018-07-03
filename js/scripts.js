49b5eb3efac006393212029a1f7b62c3b3be4b9b
keitmark <keitmark@mail.ru>
2018-07-03 11:30:00 +0300

    lalal

---
+++ js/scripts.js
@@ -0,0 +1,16 @@
+$(document).ready(function () {
+
+   $('.slider-init').slick({
+  centerMode: true,
+  centerPadding: '0',
+  slidesToShow: 3,
+       dots: true
+
+});
+    
+    
+    
+
+});
+
+
